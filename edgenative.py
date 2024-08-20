0343767ff3beb7f5581f9dcfa3464dfcb06f16ca
ccie7599 <bapley@akamai.com>
2024-08-20 07:51:08 -0400

    Update edgenative.py

--- edgenative.py
+++ edgenative.py
@@ -1,12 +1,15 @@
 from locust import HttpUser, TaskSet, task, between
 
 class UserBehavior(TaskSet):
+    host = "https://workshop.connected-cloud.io/"
     @task(2)
     def get_price_edgenative(self):
+        self.client.headers.update({"User-Agent": "edgenative"})
         # Testing the endpoint every second
         self.client.get("/get?topic=price&origin=edgenative")
-    @task(2)
+    @task(1)
     def get_price_legacy(self):
+        self.client.headers.update({"User-Agent": "legacy"})
         # Testing the endpoint every second
         self.client.get("/get?topic=price&origin=legacy")
 class WebsiteUser(HttpUser):

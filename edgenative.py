e7856d211cf7fbe9460e7d08a43aa5f348babe49
ccie7599 <bapley@akamai.com>
2024-08-20 07:25:39 -0400

    Update edgenative.py

--- edgenative.py
+++ edgenative.py
@@ -1,11 +1,14 @@
 from locust import HttpUser, TaskSet, task, between
 
 class UserBehavior(TaskSet):
-    @task
-    def get_price(self):
+    @task(2)
+    def get_price_edgenative(self):
         # Testing the endpoint every second
         self.client.get("/get?topic=price&origin=edgenative")
-
+    @task(2)
+    def get_price_legacy(self):
+        # Testing the endpoint every second
+        self.client.get("/get?topic=price&origin=legacy")
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
     # Sets the time between requests; here it's 1 second
